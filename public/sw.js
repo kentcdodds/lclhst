5a0d363401f378351cf56f07cba9f7b59f51af5b
Kent C. Dodds <me+github@kentcdodds.com>
2023-01-31 22:01:13 -0700

    catch error

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 console.log("Service Worker Loaded...");
 self.addEventListener("fetch", async (e) => {
   console.log(e);
-  const res = await fetch("http://localhost:3000");
+  const res = await fetch("http://localhost:3000").catch((e) => e);
   console.log(res);
   // e.respondWith(); //whatever you want
 });
