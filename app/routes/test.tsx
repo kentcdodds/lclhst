60a917ab6ba6bd8fc7ba4653e3b4a3302bc42c5f
Kent C. Dodds <me+github@kentcdodds.com>
2023-02-01 11:51:08 -0700

    fix test

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,8 +1,27 @@
+import type { DataFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+function getDomainUrl(request: Request) {
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  if (!host) {
+    throw new Error("Could not determine domain URL.");
+  }
+  const protocol = host.includes("localhost") ? "http" : "https";
+  return `${protocol}://${host}`;
+}
+
+export async function loader({ request }: DataFunctionArgs) {
+  return json({ domainUrl: getDomainUrl(request) });
+}
+
 export default function Test() {
+  const data = useLoaderData<typeof loader>();
   return (
     <iframe
-      src="http://localhost:2999/go?url=http%3A%2F%2Flocalhost%3A3000%2Fexercise%2F1"
-      title="localhost"
+      src={`${data.domainUrl}/go?url=http%3A%2F%2Flocalhost%3A3000%2Fexercise%2F1`}
+      title="The embedded page"
       style={{
         position: "fixed",
         top: "0",
